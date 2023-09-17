50eb37b657c6d1487382d4b8d3ea1394b7d8322b
shlyzhas <8willox8@gmail.com>
2023-09-17 21:53:59 +0300

    added annotation to first task

--- 1/1.py
+++ 1/1.py
@@ -1,10 +1,15 @@
+from typing import List
+
+class Solution:
+    def price_of_affordable_gun(self, wallet: int, prices: List[int]) -> int:
+        affordable_price = 0
+        for price in prices:
+            if wallet - price > 0 and \
+                price > affordable_price:
+                affordable_price = price
+        return affordable_price
+
 _, wallet = map(int, input().split(' '))
 prices = list(map(int, input().split(' ')))
-
-price_of_affordable_gun = 0
-for price in prices:
-    if wallet - price > 0 and \
-        price > price_of_affordable_gun:
-        price_of_affordable_gun = price
-    
-print(price_of_affordable_gun)
+solution = Solution()
+print(solution.price_of_affordable_gun(wallet, prices))
