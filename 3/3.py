0eed473ccc646db30041b714efc72dcb1c509512
shlyzhas <8willox8@gmail.com>
2023-09-17 22:27:55 +0300

    updated readme and added annotation to third task

--- 3/3.py
+++ 3/3.py
@@ -1,18 +1,36 @@
-count = int(input())
+from typing import List
+class Solution:
+    def best_combination(self, cards: List[int], combination: List[int]):
+        # def left_exp_search(left: int) -> int:
+        #     while left < len(cards) and cards[left] == combination[left]:
+        #         left <<=  1
+        #     left >>= 1
+        #     return left
+    
+        # def right_exp_search(right: int) -> int:
+        #     while right > 1 and cards[right] == combination[right]:
+        #         right >>=  1
+        #     right <<= 1
+        #     return right
+        
+        i = 0 #left_exp_search(1)
+        j = len(cards) - 1 #right_exp_search(len(cards) - 1)
+
+        while cards[i] == combination[i]:
+            i += 1
+
+        while cards[j] == combination[j]:
+            j -= 1
+        cards[i: j + 1] = sorted(cards[i: j + 1])
+
+        if cards == combination:
+            return 'YES'
+        else:
+            return 'NO'
+
+solution = Solution()
+_ = int(input())
 cards = list(map(int, input().split(' ')))
 combination = list(map(int, input().split(' ')))
 
-i = 0
-j = len(cards) - 1
-
-while cards[i] == combination[i]:
-    i += 1
-
-while cards[j] == combination[j]:
-    j -= 1
-
-cards[i:j + 1].sort()
-if cards == combination:
-    print('YES')
-else:
-    print('NO')
+print(solution.best_combination(cards, combination))
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Были решены все задачи из экзамена по программированию на Тинькофф стажировку.
 Задачи решались с использованием языка python.
 
-* [A.Линейный поиск](https://github.com/MupLever/TinkoffContest/tree/master/1)
-* [B.Хеш-таблица](https://github.com/MupLever/TinkoffContest/tree/master/2)
-* [C.Сужение поиска](https://github.com/MupLever/TinkoffContest/tree/master/3)
-* [D.Лучшая комбинация](https://github.com/MupLever/TinkoffContest/tree/master/4)
-* [E.Мультиграф](https://github.com/MupLever/TinkoffContest/tree/master/5)
-* [F.Логирование](https://github.com/MupLever/TinkoffContest/tree/master/6)
+* [A. Линейный поиск](https://github.com/MupLever/TinkoffContest/tree/master/1)
+* [B. Хеш-таблица](https://github.com/MupLever/TinkoffContest/tree/master/2)
+* [C. Сужение поиска](https://github.com/MupLever/TinkoffContest/tree/master/3)
+* [D. Лучшая комбинация](https://github.com/MupLever/TinkoffContest/tree/master/4)
+* [E. Мультиграф](https://github.com/MupLever/TinkoffContest/tree/master/5)
+* [F. Логирование](https://github.com/MupLever/TinkoffContest/tree/master/6)
