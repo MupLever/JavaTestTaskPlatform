c3720aa288723a271fc86336dd0f18a7428387ef
shlyzhas <8willox8@gmail.com>
2023-09-17 21:58:59 +0300

    added annotation to second task

--- 2/2.py
+++ 2/2.py
@@ -1,9 +1,15 @@
-letters = { letter : 0 for letter in 'sherif'}
+class Solution:
+    def max_number_of_words(self, s: str) -> int:
+        letters = { letter : 0 for letter in 'sherif'}
+
+        for char in s:
+            letters[char] += 1
+
+        letters['f'] //= 2
+    
+        return min(letters.values())
+    
 input_str = input()
+solution = Solution()
 
-for char in input_str:
-    letters[char] += 1
-
-letters['f'] //= 2
-
-print(min(letters.values()))
+print(solution.max_number_of_words(input_str))
